a35a547a0fdd139ad9e701d40ccfa6b4b6c37cad
Linh Nhi <90383599+LinhNhi1710@users.noreply.github.com>
2022-07-01 01:54:56 +0700

    Update login.php

--- login.php
+++ login.php
@@ -81,7 +81,7 @@ if(isset($message)){
       <input type="email" name="email" class="box" placeholder="enter your email" required>
       <input type="password" name="pass" class="box" placeholder="enter your password" required>
       <input type="submit" class="btn" name="submit" value="login now">
-      <p>don't have an account? <a href="register.php">register now</a></p>
+      <p>don't have an account? <a href="sign_up.php">register now</a></p>
    </form>
 
 </section>
